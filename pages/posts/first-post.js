955b5bf3233558bfb9d50b572f143c71d0ba8343
Chiro <sachinsinghroxx@gmail.com>
2021-07-10 18:37:19 +0530

    added css

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,10 +1,11 @@
 import Link from "next/link";
 import Image from "next/image";
 import Head from "next/head";
+import Layout from "../../components/layout";
 
 export default function FirstPost() {
   return (
-    <>
+    <Layout>
       <Head>
         <title>First Post</title>
       </Head>
@@ -20,6 +21,6 @@ export default function FirstPost() {
           <a>Back to home</a>
         </Link>
       </h2>
-    </>
+    </Layout>
   );
 }
