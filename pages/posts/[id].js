9cc3a1b7568efe83e301d5f3de6529704be830ed
Chiro <sachinsinghroxx@gmail.com>
2021-07-12 15:09:36 +0530

    added dynmaic routes with remark

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -1,5 +1,15 @@
 import Layout from "../../components/layout";
-import { getAllPostIds } from "../../lib/posts";
+import { getAllPostIds, getPostData } from "../../lib/posts";
+
+export async function getStaticProps({ params }) {
+  const postData = await getPostData(params.id)
+
+  return {
+    props: {
+      postData
+    }
+  }
+}
 
 export async function getStaticPaths() {
   const paths = getAllPostIds();
@@ -9,6 +19,17 @@ export async function getStaticPaths() {
   };
 }
 
-export default function Post() {
-  return <Layout>...</Layout>;
+export default function Post({ postData }) {
+  return (
+    <Layout>
+      {postData.title}
+      <br />
+      {postData.id}
+      <br />
+      {postData.date}
+      <br />
+      <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+
+    </Layout>
+  );
 }
